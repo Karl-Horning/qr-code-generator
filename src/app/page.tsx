c3707985d1b0e99f7a713a7f6b78e37c6c823d73
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-04-28 19:26:45 +0100

    Add example QR code generator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import QrCode from "@/components/QrCode/QrCode";
+
 export default function Home() {
-    return <main className="">Hello, World!</main>;
+    return (
+        <main className="container mx-auto">
+            <div className="grid grid-cols-2">
+                <QrCode />
+            </div>
+        </main>
+    );
 }
