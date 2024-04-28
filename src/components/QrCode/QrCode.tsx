afcc487138848b05794cd2024c83c0f217fba6ba
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-04-28 23:01:32 +0100

    Break up props

--- src/components/QrCode/QrCode.tsx
+++ src/components/QrCode/QrCode.tsx
@@ -1,68 +1,59 @@
-// Choose error correction code (ECC) parameter (&ecc=H)
-// L (low, ~7% destroyed data may be corrected)
-// M (middle, ~15% destroyed data may be corrected)
-// Q (quality, ~25% destroyed data may be corrected)
-// H (high, ~30% destroyed data may be corrected)
-
-// Choose hex value colour
-// https://api.qrserver.com/v1/create-qr-code/?data=Hello&size=600x600&ecc=H&color=ff0000
-
-// Choose background hex value colour
-// https://api.qrserver.com/v1/create-qr-code/?data=Hello&size=600x600&ecc=H&color=ff0000&bgcolor=000000
-
-// Add a margin from 1 to 50
-// https://api.qrserver.com/v1/create-qr-code/?data=Hello&size=600x600&ecc=H&color=ff0000&bgcolor=000000&margin=50
-
-// Choose format for code:
-// png
-// gif
-// jpeg
-// jpg
-// svg
-// eps
-// https://api.qrserver.com/v1/create-qr-code/?data=Hello&size=600x600&ecc=H&color=ff0000&bgcolor=000000&margin=50&qzone-100&format=png
-
-// Minimum 100x100
-// Maximum if png|gif|jpeg|jpg 1000x1000
-// Maximum if svg|eps 1000000x1000000
-// Choose height and width
-
-// Choose text
-// (Between 1 to 900 characters)
-
-// Encode text
-
-// Send get request to http(s)://api.qrserver.com/v1/create-qr-code/?data=[URL-encoded-text]&size=[pixels]x[pixels]
-// https://api.qrserver.com/v1/create-qr-code/?data=Hello&size=500x500
-
-// export default function Home() {
-//     return (
-//         <main className="container mx-auto">
-//             <div className="grid grid-cols-2">
-//                 Test!
-//             </div>
-//         </main>
-//     );
-// }
-
 "use client";
 
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Image from "next/image";
 
-export default function QrCode() {
+interface QrCodeProps {
+    // Choose text
+    // (Between 1 to 900 characters)
+    text?: string;
+    // Choose error correction code (ECC) quality
+    // L (low, ~7% destroyed data may be corrected)
+    // M (middle, ~15% destroyed data may be corrected)
+    // Q (quality, ~25% destroyed data may be corrected)
+    // H (high, ~30% destroyed data may be corrected)
+    ecc?: "L" | "M" | "Q" | "H";
+    color?: string;
+    bGcolor?: string;
+    // From 1 to 50
+    margin?: number;
+    // Image format:
+    // png
+    // gif
+    // jpeg
+    // jpg
+    // svg
+    // eps
+    format?: "png" | "gif" | "jpeg" | "jpg" | "svg" | "eps";
+    // Minimum 100x100
+    // Maximum if png|gif|jpeg|jpg 1000x1000
+    // Maximum if svg|eps 1000000x1000000
+    // Choose height and width
+    height?: number;
+    width?: number;
+}
+
+export default function QrCode({
+    text = "https://karl-horning.github.io",
+    ecc = "L",
+    color = "000000",
+    bGcolor = "ffffff",
+    margin = 25,
+    format = "svg",
+    height = 300,
+    width = height,
+}: QrCodeProps) {
     const [qrCodeImage, setQRCodeImage] = useState<string | null>(null);
+
+    const reqUrl = `https://api.qrserver.com/v1/create-qr-code/?data=${text}&size=${height}x${width}&ecc=${ecc}&color=#${color}&bgcolor=#${bGcolor}&margin=${margin}&qzone-100&format=${format}`;
 
     useEffect(() => {
         // Make a GET request to the QR code API endpoint
         axios
-            .get(
-                "https://api.qrserver.com/v1/create-qr-code/?data=Hello&size=600x600&ecc=H&color=000000&bgcolor=ffffff&margin=25&qzone-100&format=png",
-                {
-                    responseType: "arraybuffer", // Set response type to arraybuffer
-                }
-            )
+            .get(reqUrl, {
+                responseType: "arraybuffer", // Set response type to arraybuffer
+            })
             .then((response) => {
                 // Handle successful response
                 console.log("response:", response);
