5979a4cc20d409b7048ad6da2ded8c51de1063a6
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-05-01 22:42:13 +0100

    Add data for ECC and format selects

---
+++ src/components/QrCode/data.js
@@ -0,0 +1,61 @@
+export const eccList = [
+    {
+        label: "L",
+        value: "L",
+        description: "Approximately 7% of the codewords can be restored.",
+    },
+    {
+        label: "M",
+        value: "M",
+        description: "Approximately 15% of the codewords can be restored.",
+    },
+    {
+        label: "Q",
+        value: "Q",
+        description: "Approximately 25% of the codewords can be restored.",
+    },
+    {
+        label: "H",
+        value: "H",
+        description: "Approximately 30% of the codewords can be restored.",
+    },
+];
+
+export const formatList = [
+    {
+        label: "PNG",
+        value: "png",
+        description:
+            "Portable Network Graphics: Lossless compression format suitable for images with sharp edges and transparency.",
+    },
+    {
+        label: "GIF",
+        value: "gif",
+        description:
+            "Graphics Interchange Format: Supports animations and a limited color palette, commonly used for simple animations and logos.",
+    },
+    {
+        label: "JPEG",
+        value: "jpeg",
+        description:
+            "Joint Photographic Experts Group: Lossy compression format ideal for photographs and realistic images.",
+    },
+    {
+        label: "JPG",
+        value: "jpg",
+        description:
+            "Joint Photographic Group: Common file extension for JPEG image files.",
+    },
+    {
+        label: "SVG",
+        value: "svg",
+        description:
+            "Scalable Vector Graphics: XML-based vector image format suitable for graphics that need to scale without losing quality.",
+    },
+    {
+        label: "EPS",
+        value: "eps",
+        description:
+            "Encapsulated PostScript: Graphics file format suitable for printing vector graphics.",
+    },
+];
