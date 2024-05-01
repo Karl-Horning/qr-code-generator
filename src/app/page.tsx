5b309eef72be76d96902e0a528995c5c079a71e6
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-05-01 22:42:35 +0100

    Add background gradient to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import QrCode from "@/components/QrCode/QrCode";
 
 export default function Home() {
     return (
-        <main>
+        <main className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 py-8">
             <QrCode />
         </main>
     );
